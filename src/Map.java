4572bc261965b17993a43919a4c60a84f5374008
iMTILab <imtilab@users.noreply.github.com>
2017-03-02 01:22:54 +0600

    updated A star Search Map.java

--- src/Map.java
+++ src/Map.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,23 +14,26 @@ import java.util.Scanner;
  * @author mti
  */
 public class Map {
-    Queue<Integer> queue = new LinkedList<>();
 
-    double[][] graph_matrix;
-    double distance[];
-    int parent[];
-    int source, destination;
-//------------------------------------------
-    int cities;//number of cities
-    int roads;//number of roads
-    String city_list[];//city name list
-    String start;//start city name
-    String end;//destination city name
+    Queue<Integer> openList_queue = new LinkedList<>();
+    int closedList[];
+    int closed_index = 1;
 
     double[] huristics;//Straight-Line-Distance (huristics)
-    int openList[];
-    int closedList[];
-    int cityValueToAssigned;//city value assign counter
+    double gn[];//known path cost so far from start to n.
+    double fn[];//Heuristic, estimate cost from n to the destination.
+
+    int cities;//number of cities
+    int roads;//number of roads
+    String start;//start city name
+    String end;//destination city name
+    String city_list[];//city name list
+
+    double[][] graph_matrix;
+    int parent[];
+    int source, destination;
+
+    int cityValue_toAssign_index;//city value assign index
 
     //set and initialize the full graph_matrix
     public void setGraph() throws Exception {
@@ -48,39 +50,38 @@ public class Map {
         end = bufferedReader.readLine();
 
         //initializations
-        graph_matrix = new double[cities+1][cities+1];
-        city_list = new String[cities+1];
-        openList = new int[cities+1];
-        closedList = new int[cities+1];
-        huristics = new double[cities+1];
-        
-        distance = new double[cities + 1];
+        graph_matrix = new double[cities + 1][cities + 1];
+        city_list = new String[cities + 1];
+        closedList = new int[cities + 1];
+        huristics = new double[cities + 1];
+        gn = new double[cities + 1];
+        fn = new double[cities + 1];
+
         parent = new int[cities + 1];
 
         //input all pairs of city names and assign weights
-        lineArray = new String[3];
-        cityValueToAssigned = 1;
+        cityValue_toAssign_index = 1;
         for (int i = 0; i < roads; i++) {
             stringLine = bufferedReader.readLine();
             lineArray = stringLine.split(" ");
             //System.out.println(lineArray[0]+" "+lineArray[1]+" "+lineArray[2]);
 
-            int row = cityValueToAssigned;
+            int row = cityValue_toAssign_index;
             int cityValue = checkCityValueAssigned(lineArray[0]);
 
             if (cityValue < 0) {//cityValue not assigned yet
-                city_list[cityValueToAssigned] = lineArray[0];
-                cityValueToAssigned++;
+                city_list[cityValue_toAssign_index] = lineArray[0];
+                cityValue_toAssign_index++;
             } else {//assigned
                 row = cityValue;
             }
 
-            int col = cityValueToAssigned;
+            int col = cityValue_toAssign_index;
             cityValue = checkCityValueAssigned(lineArray[1]);
 
             if (cityValue < 0) {
-                city_list[cityValueToAssigned] = lineArray[1];
-                cityValueToAssigned++;
+                city_list[cityValue_toAssign_index] = lineArray[1];
+                cityValue_toAssign_index++;
             } else {
                 col = cityValue;
             }
@@ -90,7 +91,6 @@ public class Map {
         }
 
         //input huristics for the cities
-        //  lineArray = new String[2];
         for (int i = 0; i < cities; i++) {
             stringLine = bufferedReader.readLine();
             lineArray = stringLine.split(" ");
@@ -108,46 +108,62 @@ public class Map {
         destination = checkCityValueAssigned(end);//assign value of end city
     }
 
-    public void Dijkstra() {
-        for (int i = 1; i < distance.length; i++) {//initial distance infinity
-            distance[i] = 99999;
-        }
+    //A start algo
 
-        distance[source] = 0;
-        for (int v = 1; v <= cities; v++) {
-            queue.add(v);
-        }
+    public void A_star() {
+        openList_queue.add(source);
+        fn[source] = 0;//not important as initially all 0
 
-        Dijkstra_visit();
+        A_star_visit();
 
         printResult();
     }
 
-    public void Dijkstra_visit() {
+    public void A_star_visit() {
 
-        while (!queue.isEmpty()) {
-            Priority.setPriority(queue, distance);
-            int ParentVer = (int) queue.poll();
+        while (!openList_queue.isEmpty()) {
+            Priority.setPriority(openList_queue, fn);//sort queue based on fn value
+            int parentVer = (int) openList_queue.poll();
 
-            //check for all adjacency vertices of the visisted vertex
-            for (int adjacent = 1; adjacent < graph_matrix[0].length; adjacent++) {
-                if (graph_matrix[ParentVer][adjacent] > 0) {//weight exists
-                    if (distance[adjacent] > distance[ParentVer] + graph_matrix[ParentVer][adjacent]) {
-                        distance[adjacent] = distance[ParentVer] + graph_matrix[ParentVer][adjacent];
-                        parent[adjacent] = ParentVer;//keeps parent ver
-                    }
+            if (parentVer == destination) {//reached to destination
+                break;
+            }
+
+            //check for all adjacency vertices of the parent vertex
+            for (int adjacentVer = 1; adjacentVer < graph_matrix[0].length; adjacentVer++) {
+
+                if (graph_matrix[parentVer][adjacentVer] > 0 && !isClosed(adjacentVer)) {//weight exists and not closed
+
+                    gn[adjacentVer] = gn[parentVer] + graph_matrix[parentVer][adjacentVer];
+                    fn[adjacentVer] = huristics[adjacentVer] + gn[adjacentVer];
+
+                    parent[adjacentVer] = parentVer;//keeps parent ver
+                    openList_queue.add(adjacentVer);//add to open list
                 }
             }
+            closedList[closed_index++] = parentVer;//add to closed list
         }
+    }
+
+    //take a node value, 
+    //if it is in closed list return true otherwise return false
+
+    public boolean isClosed(int adjacent) {
+        for (int i = 1; i < closed_index; i++) {
+            if (closedList[i] == adjacent) {
+                return true;
+            }
+        }
+        return false;
     }
 
     //print distance and path
     public void printResult() {
         //cost from source to each node
-//        for (int c = 2; c < distance.length; c++) {
-//            System.out.println("distance from " + source + " to " + c + ": " + distance[c]);
+//        for (int c = 2; c < gn.length; c++) {
+//            System.out.println("distance from " + start + " to " + city_list[c] + ": " + gn[c]);
 //        }
-        
+
         //path from source to destination
         int dest = destination;
         String path = "" + city_list[dest];
@@ -158,15 +174,15 @@ public class Map {
                 break;
             }
         }
-        System.out.println("Shortest path: "+path);
-        
-        System.out.println("Cost from "+start+" to "+end+": "+distance[destination]);
+        System.out.println("Shortest path: " + path);
+
+        System.out.println("Cost from " + start + " to " + end + ": " + gn[destination]);
     }
-    
+
     //take a city name and check if it is already assigned with a value
     //if assigned, return the assigned number, otherwise return -1
     public int checkCityValueAssigned(String city_name) {
-        for (int i = 1; i < cityValueToAssigned; i++) {
+        for (int i = 1; i < cityValue_toAssign_index; i++) {
             if (city_list[i] != null) {
                 if (city_list[i].equals(city_name)) {
                     return i;
